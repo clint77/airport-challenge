67c535bdcfe89fb8f20d2f1f4c680bc37d663d0c
Clint <clintp1@gmail.com>
2014-12-18 18:35:23 +0000

    refactored finale spec and pass test

--- lib/finale.rb
+++ lib/finale.rb
@@ -1,5 +1,5 @@
 class Finale
 
-End
+  
 
-
+end
--- spec/finale_spec.rb
+++ spec/finale_spec.rb
@@ -1,13 +1,33 @@
+require 'finale'
+require 'airport'
+require 'plane'
+
 describe Finale do
+
+  let(:plane){Plane.new}
+  let(:airport){Airport.new}
+
   
-  it 'can land all 6 plane' do
-    6.times 
+  it 'all planes can land and all planes can take off' do
+    allow(airport).to receive(:weather_storm_generator?).and_return(true)
+    6.times { airport.accept_landing(plane) }
+    expect(plane).not_to be_flying
+    expect(airport.plane_count).to eq 6
+    6.times { airport.take_off_airport(plane) }
+    expect(plane).to be_flying
+    expect(airport.plane_count).to eq 0
   end
+  
+  # it 'can land all 6 plane' do
+  #   airport = Airport.new
+  #   plane = Plane.new
+  #   allow(airport).to receive(:weather_storm_generator?).and_return(true)
+  #   airport.accept_landing(plane)
+  #   expect(airport.).to eq 6
+  # end
 
-  it 'can take off all plane when airport is full' do
+  # it 'can take off all plane when airport is full' do
 
-  end
-
-  it ''
+  # end
 
 end
